4ed36b50fa4b81115742fbef5a09c91d5c5c8b1a
testproject-testing-cf <56145975+zyrill707@users.noreply.github.com>
2024-03-24 21:48:53 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById("yes").onclick = function(){
+  window.location.href = "yes.html";
+}
+
+document.getElementById("no").onclick = function(){
+  window.location.href = "no.html";
+}
